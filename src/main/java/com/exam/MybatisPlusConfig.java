7d027b9b4cd93a602f75829f22c80313745275f1
sunhuadi <shd_student@163.com>
2021-01-04 15:07:47 +0800

    Initial commit

---
+++ src/main/java/com/exam/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.exam;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configuration
+//@MapperScan(basePackages = {"com.exam.mapper"})
+@MapperScan("com.exam.mapper")
+//@MapperScan("com.exam.service.*.mapper*")
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
